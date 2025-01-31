d0ef8ea0cbb9bfa5ec460e88db85ac767901ce29
Darren <ades96063@gmail.com>
2025-01-31 16:57:04 +0800

    Rearrange File 1
    Rearrange File 1
    1. Separate the sub-modules
    2. New dictionary created

--- Submodule/base_address_rd.v
+++ Submodule/base_address_rd.v
@@ -11,12 +11,15 @@ module base_address_rd #(
     output reg [31:0] ram_addr,
     //read
     output ram_en ,
-    input [31:0] ram_rd_data,
+    input  [31:0] ram_rd_data,
     //write(not use)
     output [3:0]  ram_we,
     output [31:0] ram_wd_data,
     //to m01_signal
-    output Transfer_Done
+    output wire Transfer_Done,
+    //user use
+    input   change_based_address
+    //input   data_get
     );
 
 assign ram_rst  = 1'b0;
@@ -26,32 +29,66 @@ assign ram_we   = 4'b0;
 assign ram_wd_data = 32'd0;
 
 
-reg counter;
-assign Transfer_Done    =   (counter) ? 1'b1 : 1'b0;
+reg         counter;
+reg [31:0]  reg_ram_rd_data;
 
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        counter <= 1'b0;    
-    end 
-    else begin
-      if (counter) begin
-        counter <= counter;        
-      end
-      else begin
-        counter <= counter + 1'b1;
-      end
+reg [31:0]  data_input_change;
+
+// change to pulse
+assign Transfer_Done = (reg_ram_rd_data == data_input_change) ? 1'b1 : 1'b0;//(counter && ((&ram_rd_data) != 0)) ? 1'b1 : 1'b0;
+//   assign Transfer_Done  =  (counter == 1'b1) ? 1'b1 : 1'b0;
+//--------------------- rd data reg ---------------------//
+    always @(posedge clk  or negedge rst_n) begin
+        if (!rst_n) begin
+            // data_input_change <=  32'h0001_0010;
+            data_input_change <=  32'h0001_0030;
+        end 
+        else if(Transfer_Done)begin
+            // data_input_change <=  32'h0003_0010;    
+            // data_input_change <=  data_input_change - 32'h0001_0000;        
+            data_input_change <=  data_input_change + 32'h0001_0000;        
+        end 
     end
-end
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            reg_ram_rd_data <=  32'd0;
+        end 
+        else begin
+            reg_ram_rd_data <=  ram_rd_data;            
+        end
+    end
+
+
+    // always @(posedge clk or negedge rst_n) begin
+    //     if (!rst_n) begin
+    //         counter <= 1'b0;    
+    //     end 
+    //     else begin
+    //         if ((counter == 1'b1) && (!Transfer_Done)) begin
+    //             counter <= counter;        
+    //         end
+    //         else if (change_based_address)  begin
+    //             counter <=  1'b0;
+    //         end
+    //         else if (reg_ram_rd_data == data_input_change) begin
+    //             counter <= counter + 1'b1;
+    //         end
+    //         // else 
+    //         //     counter <=  counter + 1'b1;
+    //     end
+    //end
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
-        ram_addr <= 32'd0;
+        ram_addr <= START_ADDR;
     end
     else begin
-        if(counter == 0)
-            ram_addr    <= START_ADDR;
+        if (change_based_address)  begin
+            ram_addr    <= ram_addr +   32'h0000_0004;
+        end
         else
-            ram_addr    <= 32'd0;
+            ram_addr    <= ram_addr;
     end
 end
 
